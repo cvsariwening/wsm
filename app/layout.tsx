bd4a1823d6b4c47688bbe2f2efc73a03c490abd0
cvsariwening <cvsariwening01@gmail.com>
2025-05-07 15:39:54 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,13 +25,6 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
-      <script async src='https://d2mpatx37cqexb.cloudfront.net/delightchat-whatsapp-widget/embeds/embed.min.js'></script>
-      <script>
-        var wa_btnSetting = {"btnColor":"#16BE45","ctaText":"WhatsApp Us","cornerRadius":40,"marginBottom":20,"marginLeft":20,"marginRight":20,"btnPosition":"right","whatsAppNumber":"6281213478092","welcomeMessage":"Selamat datang","zIndex":999999,"btnColorScheme":"light"};
-        window.onload = () => {
-          _waEmbed(wa_btnSetting);
-        };
-      </script>
       </body>
     </html>
   )
