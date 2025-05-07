9a8b855ff36f7b61d645018379740ab9c0327392
cvsariwening <cvsariwening01@gmail.com>
2025-05-07 13:34:54 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,6 @@ export default function RootLayout({
           {children}
         </ThemeProvider>
       </body>
-        <h1>wa floating button</h1>
         <a href="https://wa.me/6281213478092?text=Salam%20sukses🤝%20saya%20ingin%20bertanya%20terkait%20produk%20CV.SARI%20WENING">Chat whatsapp</a>
     </html>
   )
